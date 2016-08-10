5f4130999dfd0380e913dd1bf3bd125509a135be
dmitrime <dmitrime@github.com>
2016-08-10 15:38:17 +0700

    tx controller

---
+++ app/controllers/TransactionController.scala
@@ -0,0 +1,46 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+
+import services.TransactionService
+import models.Transaction
+
+/**
+ * Controller with injected [[TransactionService]] for handling 
+ * transaction operations.
+ */
+@Singleton
+class TransactionController @Inject() (transactionService: TransactionService) extends Controller {
+
+  /**
+   * Action that stores a new transaction.
+   */
+  def store(transactionId: Long) = Action(parse.json) { request => 
+    val tx = (request.body.as[JsObject] + ("transaction_id" -> Json.toJson(transactionId))).as[Transaction]
+    transactionService.store(tx)
+    Ok(Json.obj("status" -> "ok")) 
+  }
+  
+
+  /**
+   * Action that retrieves a transaction by id.
+   */
+  def retrieve(transactionId: Long) = Action {
+    System.out.println(transactionService.retrieve(transactionId))
+    Ok(Json.toJson(transactionService.retrieve(transactionId)))
+  }
+
+  /**
+   * Action that retrieves a transaction by id.
+   */
+  def listType(typeName: String) = Action { Ok(Json.toJson(transactionService.listType(typeName))) }
+
+  /**
+   * Action that retrieves a transaction by id.
+   */
+  def sum(transactionId: Long) = Action { Ok(Json.toJson(4.2)) }
+
+}
